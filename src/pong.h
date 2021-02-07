db29866d39c777842e1b95e3ea410bb61c2294ac
ZeddZoo <ziyizuo@andrew.cmu.edu>
2021-02-06 22:48:21 -0500

    ponggopngpogngopn

---
+++ src/pong.h
@@ -0,0 +1,11 @@
+typedef struct {
+  int lposX, lposY;
+  int rposX, rposY;
+  int bposX, bposY;
+} pongData_t;
+
+// class Pong {
+//   int lposX, lposY;
+//   int  rposX, rposY;
+//   int bposX, bposY;
+// };
