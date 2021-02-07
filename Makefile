838b873e97ebb2b9361b57cc937e550b6f10c24d
alicel@andrew.cmu.edu <alice@kefrobotics.com>
2021-02-06 23:22:12 -0500

    read from doom working

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build18: main.cpp
-	clang++ -O3 -lopencv_core -lopencv_imgcodecs -lportaudio -I/usr/include/opencv4 -o build18 main.cpp
+	clang++ -O3 -I/usr/include/opencv2 -o build18 main.cpp -lopencv_core -lopencv_imgcodecs -lportaudio -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lX11
--- main.cpp
+++ main.cpp
@@ -4,12 +4,20 @@
 #include <stdlib.h>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include "opencv2/imgproc.hpp"
 #include <opencv2/highgui.hpp>
+#include "opencv2/opencv.hpp"
 #include <sys/types.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <cstdint>
+#include <cstring>
+#include <vector>
 
 #define SAMPLE_RATE (120000)
-#define WIDTH (80)
-#define HEIGHT (80)
+#define WIDTH (100)
+#define HEIGHT (100)
+#define PIXEL_THRESH (80)
 
 typedef struct {
   float left_phase;
@@ -22,18 +30,47 @@ typedef struct {
 
 using namespace cv;
 
+//std::string video_path = "../../Downloads/loop_of_fire_flames.mp4";
+std::string video_path = "../../Downloads/mandelbrot.mp4";
+VideoCapture cap(video_path);
 Mat img;
 int x = 0, xd = 1, y = 0;
+int Width = 0;
+int Height = 0;
+int Bpp = 0;
+
+void ImageFromDisplay(std::vector<uint8_t>& Pixels, int& Width, int& Height, int& BitsPerPixel)
+{
+    Display* display = XOpenDisplay(nullptr);
+    Window root = DefaultRootWindow(display);
+
+    XWindowAttributes attributes = {0};
+    XGetWindowAttributes(display, root, &attributes);
+
+    Width = attributes.width;
+    Height = attributes.height;
+
+    XImage* ximg = XGetImage(display, root, 0, 0 , Width, Height, AllPlanes, ZPixmap);
+    BitsPerPixel = ximg->bits_per_pixel;
+    Pixels.resize(Width * Height * 4);
+
+    memcpy(&Pixels[0], ximg->data, Pixels.size());
+    XDestroyImage(ximg);
+    XCloseDisplay(display);
+}
 
 static int patestCallback(const void *inputBuffer, void *outputBuffer,
                           unsigned long framesPerBuffer,
                           const PaStreamCallbackTimeInfo *timeInfo,
                           PaStreamCallbackFlags statusFlags, void *userData) {
+
   /* Cast data passed through stream to our structure. */
   paTestData *data = (paTestData *)userData;
   float *out = (float *)outputBuffer;
+  Mat raw_img;
   unsigned int i;
   (void)inputBuffer; /* Prevent unused variable warning. */
+  std::vector<std::uint8_t> Pixels;
 
   for (i = 0; i < framesPerBuffer; i++) {
     // find next dark pixel
@@ -50,15 +87,29 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
         y += 1;
       }
     }
+
+
     if (y >= HEIGHT) {
       y = 0;
+      //cap >> raw_img;
+      //cvtColor(raw_img, raw_img, CV_BGR2GRAY);
+      std::vector<std::uint8_t> Pixels;
+      ImageFromDisplay(Pixels, Width, Height, Bpp);
+      if (Width && Height)
+      {
+          Mat raw_img = Mat(Height, Width, Bpp > 24 ? CV_8UC4 : CV_8UC3, &Pixels[0]); //Mat(Size(Height, Width), Bpp > 24 ? CV_8UC4 : CV_8UC3, &Pixels[0]);
+          cvtColor(raw_img, raw_img, CV_BGR2GRAY);
+          resize(raw_img, img, Size(WIDTH,HEIGHT), 0, 0, CV_INTER_LINEAR);
+      }
+      //resize(raw_img, img, Size(WIDTH,HEIGHT), 0, 0, CV_INTER_LINEAR);
     }
+
 
     *out++ = data->left_phase;  /* left */
     *out++ = data->right_phase; /* right */
 
     Scalar b = img.at<u_int8_t>(Point(x, y));
-    if (b[0] < 200) {
+    if (b[0] < PIXEL_THRESH) {
       continue;
     }
 
@@ -68,14 +119,14 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
   return 0;
 }
 
+
 static paTestData data;
+
 int main() {
   // open image
-  std::string image_path = samples::findFile("test.png");
+  std::string image_path = "test.png";
   img = imread(image_path, IMREAD_GRAYSCALE);
 
-
-  // connect to stream
   PaStream *stream;
   PaError err;
 
@@ -87,7 +138,7 @@ int main() {
   }
 
   if (Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE, 256,
-                           patestCallback, &data) != paNoError) {
+                          patestCallback, &data) != paNoError) {
     fprintf(stderr, "Cannot open stream\n");
     Pa_Terminate();
     exit(EXIT_FAILURE);
@@ -99,7 +150,11 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  Pa_Sleep(10000);
+  while (1) {
+    Pa_Sleep(100000);
+  }
 
   Pa_Terminate();
+  cap.release();
+  return 0;
 }
