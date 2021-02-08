3fa1cb5a5961608009bc011469465c4ddc16d648
Jeremy Dropkin <jdropkin@andrew.cmu.edu>
2021-02-08 15:32:20 -0500

    Add imgs folder and implement zooming

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#include <mutex>
 
 #define SAMPLE_RATE (120000)
 #define WIDTH (80)
@@ -26,10 +29,16 @@ using namespace cv;
 
 Mat img;
 
+std::mutex lock;
+
 unsigned xPos = 0;
 unsigned yPos = 0;
 int xDir = 1;
 unsigned zoomLvl = 1;
+unsigned xWindow = 0;
+unsigned yWindow = 0;
+
+int pipefd[2];
 
 static int patestCallback(const void *inputBuffer, void *outputBuffer,
                           unsigned long framesPerBuffer,
@@ -39,7 +48,13 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
   paTestData *data = (paTestData *)userData;
   float *out = (float *)outputBuffer;
   unsigned int i;
-  (void)inputBuffer; /* Prevent unused variable warning. */
+  (void) inputBuffer; /* Prevent unused variable warning. */
+  (void) timeInfo;
+  (void) statusFlags;
+  unsigned char c;
+  lock.lock();
+
+  read(pipefd[0], &c, sizeof(char));
 
   // Loop through frames asked for by port audio
   for (i = 0; i < framesPerBuffer; i++) {
@@ -48,28 +63,34 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
     if (xDir) {
       xPos++;
       // If x reaches end of the line, swap direction and increment y
-      if (xPos >= WIDTH/zoomLvl) {
+      if (xPos >= xWindow + WIDTH/zoomLvl) {
         xDir = 0;
         yPos++;
       }
     } else {
       xPos--;
       // If x has wrapped around, swap directions and increment y
-      if (xPos == 0) {
+      if (xPos == xWindow) {
         xDir = 1;
         yPos++;
       }
     }
 
     // If finished drawing image, go back to beginning
-    if (yPos >= HEIGHT/zoomLvl) {
-      yPos = 0;
+    if (yPos >= yWindow + HEIGHT/zoomLvl) {
+      yPos = yWindow;
     }
 
     // Output values
     *out++ = data->left_phase;  /* left */
     *out++ = data->right_phase; /* right */
 
+    if (img.empty()) {
+      data->left_phase = 0;
+      data->right_phase = 0;
+      lock.unlock();
+      return 0;
+    }
     // Get values from image
     Scalar b = img.at<u_int8_t>(Point(xPos, yPos));
     //std::cout << b << " " << xPos << " " << yPos << std::endl;
@@ -78,10 +99,11 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
     }
 
     // Normalize values
-    data->left_phase = 2.0 * (float)xPos / (float)(WIDTH/zoomLvl) - 1;
-    data->right_phase = 2.0 * (float)yPos / (float)(HEIGHT/zoomLvl) - 1;
+    data->left_phase = 2.0 * (float)(xPos - xWindow) / (float)(WIDTH/zoomLvl) - 1;
+    data->right_phase = 2.0 * (float)(yPos - yWindow) / (float)(HEIGHT/zoomLvl) - 1;
   }
 
+  lock.unlock();
   return 0;
 }
 
@@ -93,6 +115,7 @@ int main(int argc, char *argv[]) {
   bool pong_flag = false;
   bool doom_flag = false;
   bool help_flag = false;
+  bool img_flag = false;
   std::string image_path;
 
   tcgetattr(STDIN_FILENO, &old_tio);
@@ -103,9 +126,14 @@ int main(int argc, char *argv[]) {
 
   tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 
+  if (argc < 0) {
+    help_flag = true;
+  }
+
   while ((opt = getopt(argc, argv, "hi:pd")) != -1) {
     switch(opt) {
       case 'i':
+        img_flag = true;
         image_path = optarg;
         break;
       case 'p':
@@ -121,6 +149,15 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  if (help_flag) {
+    printf("Usage: ./bld/main [options]\n");
+    printf("\t-h Print this message\n");
+    printf("\t-i <path> Display an image\n");
+    printf("\t-p Play pong\n");
+    printf("\t-d Play doom\n");
+    return -1;
+  }
+
   // open image
   //std::string image_path = "test.png";
   img = imread(image_path, IMREAD_GRAYSCALE);
@@ -128,6 +165,8 @@ int main(int argc, char *argv[]) {
   // connect to stream
   PaStream *stream;
   PaError err;
+
+  pipe2(pipefd, O_NONBLOCK);
 
   data.left_phase = data.right_phase = 0.0;
 
@@ -150,30 +189,66 @@ int main(int argc, char *argv[]) {
   }
 
   while(1) {
-    char c;
     c = getchar();
+
     switch (c) {
       case 'q':
         goto done;
         break;
       case 'i':
-        zoomLvl++;
+        if (zoomLvl < 3) {
+          zoomLvl++;
+          //xWindow = 0;
+          //yWindow = 0;
+        }
         break;
       case 'o':
-        zoomLvl--;
+        if (zoomLvl > 1) {
+          zoomLvl--;
+          //xWindow = 0;
+          //yWindow = 0;
+        }
         break;
       case 'w':
+        if (yWindow > 0) {
+          yWindow -= HEIGHT / zoomLvl;
+          xPos = xWindow;
+          yPos = yWindow;
+          xDir = 1;
+        }
+        std::cout << "x: " << xWindow << " y: " << yWindow << std::endl;
         break;
       case 'a':
+        if (xWindow > 0) {
+          xWindow -= WIDTH / zoomLvl;
+          xPos = xWindow;
+          yPos = yWindow;
+          xDir = 1;
+        }
+        std::cout << "x: " << xWindow << " y: " << yWindow << std::endl;
         break;
       case 's':
+        //if ((yWindow + (HEIGHT / zoomLvl)) < HEIGHT - (HEIGHT / zoomLvl)) {
+        if (yWindow < HEIGHT - (HEIGHT / zoomLvl)) {
+          yWindow += HEIGHT / zoomLvl;
+          xPos = xWindow;
+          yPos = yWindow;
+          xDir = 1;
+        }
+        std::cout << "x: " << xWindow << " y: " << yWindow << std::endl;
         break;
       case 'd':
+        //if ((xWindow + (WIDTH / zoomLvl)) < WIDTH - (WIDTH / zoomLvl)) {
+        if (xWindow < WIDTH - (WIDTH / zoomLvl)) {
+          xWindow += WIDTH / zoomLvl;
+          xPos = xWindow;
+          yPos = yWindow;
+          xDir = 1;
+        }
+        std::cout << "x: " << xWindow << " y: " << yWindow << std::endl;
         break;
     }
-    if (c == 'q') {
-      goto done;
-    }
+    write(pipefd[1], &c, sizeof(char));
   }
 
 done:
